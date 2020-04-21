48921ecab22143fccd405761e4fb942c7a782d47
Basant Singh <you@example.com>
2020-04-21 15:21:43 +0530

    added basic angular stuff

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,7 +38,7 @@ namespace API
             services.AddCors(opt=>{
                 opt.AddPolicy("CorsPolicy",policy=>{
                     policy.AllowAnyHeader().
-                    AllowAnyMethod().WithOrigins("http://localhost:4200");
+                    AllowAnyMethod().WithOrigins("https://localhost:4200");
                 });
                 });
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Shopping';
+  products: IProduct[];
+  constructor( private http:HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?PageSize=50')
+    .subscribe((response: IPagination) => {
+      this.products = response.data;
+      console.log(response);
+    },
+    error =>{
+      console.log(error);
+    }
+    );
+  }
 }
