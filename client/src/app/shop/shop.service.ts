8a778c8777f429e62b92f27f973d8df7d9d5e23e
Basant Singh <you@example.com>
2020-04-23 18:44:42 +0530

    added products on cart sorting seahing filtering etc (but sorting products alphabetically in reverse manner)

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -29,7 +29,7 @@ export class ShopService {
       params = params.append('typeId', shopParams.typeId.toString());
     }
 
-    if(shopParams.search) {
+    if (shopParams.search) {
       params = params.append('search', shopParams.search);
     }
     params = params.append('sort', shopParams.sort);
